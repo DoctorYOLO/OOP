1ee4838686be3bee40cd0a73409ec456705545a0
DoctorYOLO <dell-95@mail.ru>
2015-05-13 12:47:35 +0300

    Исправил

--- lab1/1.2/src/bin2dec.cpp
+++ lab1/1.2/src/bin2dec.cpp
@@ -4,14 +4,6 @@
 
 using namespace std;
 
-int StringToInt(const char * str, bool & err)
-{
-	char * pLastChar = NULL;
-	int param = strtol(str, &pLastChar, 10);
-	err = ((*str == '\0') || (*pLastChar != '\0'));
-	return param;
-}
-
 int main(int argc, char* argv[])
 {
 	if (argc != 2)
@@ -19,41 +11,23 @@ int main(int argc, char* argv[])
 		printf("Wrong amount of arguments\n");
 		return 1;
 	}
-	bool err;
-	int counter = 0;
-	int binary = StringToInt(argv[1], err);
-	if (err)
-	{
-		printf("Argument is not binary number\n");
-		return 1;
-	}
-	int binary2 = binary;
-	int digit = 1;
-	while (binary2 >= 1 && digit <= 1)
-	{
-		digit = binary2 % 10;
-		binary2 /= 10;
-		counter += 1;
-	}
-	int two = 1;
+	char *binary = argv[1];
+	char digit;
+	int counter;
 	int decimal = 0;
-	binary2 = binary;
-	if (digit > 1)
+	int i;
+	int two = 0;
+	for (i = 0; i < strlen(binary); i++)
 	{
-		printf("It's not binary number!\n");
-		return 1;
-	}
-	else
-	{
-		while (counter > 0)
+		if ((binary[i] < '0') || (binary[i] > '1'))
 		{
-			digit = binary2 % 10;
-			binary2 /= 10;
-			decimal = decimal + digit * two;
-			two *= 2;
-			counter -= 1;
+			printf("It's not binary number!\n");
+			return 1;
 		}
-		cout << decimal << endl;
+		two = two * 2;
+		two = two + binary[i] - '0';
 	}
+	decimal = two;
+	printf("%d", decimal);
 	return 0;
 }
